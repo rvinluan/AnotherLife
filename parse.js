96820f996b0881ba492c737cf4033be5a8b65a81
Robert Vinluan <robert.vinluan@nytimes.com>
2015-11-06 16:41:07 -0500

    parsing into individual entries

--- parse.js
+++ parse.js
@@ -5,15 +5,31 @@ var fs = require("fs");
 
 var result;
 
-// this finds whole entries.
-// regex explanation:
-//      (date + newline) + (any text/newlines, lazy) + (date + newline OR end of string, lookahead) 
-parser.addRule(/[\d-]+\r\n\r\n[\s\S]+?(?=([\d-]+\r\n\r\n)|$)/g, function(entry) {
+/****
+//
+// REGEXES!
+//
+****/
+
+var RegExps = {
+  // this finds a date in the format
+  // YYYY-MM-DD or similar.
+  date: /[\d-]+\r\n\r\n/,
+
+  // this finds whole entries.
+  // (date + newline) + (any text/newlines, lazy) + (date + newline OR end of string, lookahead) 
+  entry: /[\d-]+\r\n\r\n[\s\S]+?(?=([\d-]+\r\n\r\n)|$)/g,
+}
+
+parser.addRule(RegExps.entry, function(entry) {
   //clean up newlines within entry
+  //separate the date
+  var date = entry.match(RegExps.date);
+  console.log(date);
   entry = entry.replace(/\r\n\r\n/g, '\n');
   return { 
     type: "entry",
-    date: "",
+    date: date,
     text: entry.trim()
   };
 });
@@ -23,5 +39,5 @@ fs.readFile('sourceText/ohlife_20141012-sanitized.txt', 'utf8', function (err, d
     result = err;
   }
   result = parser.toTree(data);
-  console.log(result.slice(0,10));
+  console.log(result.slice(0,1));
 });
